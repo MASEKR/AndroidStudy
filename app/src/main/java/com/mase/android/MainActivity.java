de30d2a5d5118f57740641037a80b89471ddd51e
Minsung Kim <leynuxms@gmail.com>
2023-03-23 14:57:09 +0900

    initial commit

---
+++ app/src/main/java/com/mase/android/MainActivity.java
@@ -0,0 +1,23 @@
+package com.mase.android;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onClicked(View v) {
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("tel:010-1234-5678"));
+        startActivity(intent);
+    }
+}
