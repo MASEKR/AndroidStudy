6b2a3d83a2301f06adb64ff17f7e6a0e98c6956a
Minsung Kim <46354958+MASEKR@users.noreply.github.com>
2023-03-30 16:48:21 +0900

    0330 upload

--- app/src/main/java/com/mase/android/MainActivity.java
+++ app/src/main/java/com/mase/android/MainActivity.java
@@ -2,19 +2,30 @@ package com.mase.android;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main2);
+        setContentView(R.layout.activity_main3);
     }
+
+    final Button button = (Button) findViewById(R.id.button);
+
+    button.setOnClickListener(new View.OnClickListener() {
+        public void onClick(View v) {
+            Toast.makeText(getApplicationContext(), "버튼이 눌렸습니다.", Toast.LENGTH_SHORT).show();
+        }
+    });
+
 
     public void onClicked(View v) {
         Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("tel:010-1234-5678"));
